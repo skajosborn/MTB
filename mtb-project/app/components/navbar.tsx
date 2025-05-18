5da9e647a9aeaae603ea49a95afd5fdc640f95fc
skajosborn <saraosborn29@yahoo.com>
2025-05-18 16:21:35 -0400

    decreased height of navbar

--- mtb-project/app/components/navbar.tsx
+++ mtb-project/app/components/navbar.tsx
@@ -71,7 +71,7 @@ export function Navbar() {
     <>
       <nav className="bg-black opacity-80 fixed top-0 left-0 right-0 z-50">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-20 items-center justify-between">
+          <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center justify-center sm:hidden">
               {/* Mobile menu button*/}
               <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded={mobileMenuOpen} onClick={() => setMobileMenuOpen((open) => !open)}>
